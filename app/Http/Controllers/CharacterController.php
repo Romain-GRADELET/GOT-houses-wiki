fd0136317767b0b4cb9f4ee762deb01b56304e2c
Romain Gradelet <121499880+Romain-GRADELET@users.noreply.github.com>
2023-11-28 13:42:17 +0100

    update comment

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -7,14 +7,14 @@ use App\Models\Character;
 class CharacterController extends Controller
 {
 
-    // Méthode permettant de récupérer l'ensemble des personnages
+    // Méthode permettant de récupérer l'ensemble des personnages.
     public static function list()
     {
         $characters = Character::all()->load('houses', 'title');
         return $characters;
     }
 
-    // Méthode permettant de récupérer un seul personnage
+    // Méthode permettant de récupérer un seul personnage.
     public static function show($id)
     {
         $character = Character::findOrFail($id)->load('houses', 'title');
